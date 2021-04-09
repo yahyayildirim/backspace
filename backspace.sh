73308d35a994f0c2b5a225df0b435f52a5074b42
Yahya YILDIRIM <63667498+yahyayildirim@users.noreply.github.com>
2021-04-09 11:45:15 +0300

    Update backspace.sh

--- backspace.sh
+++ backspace.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
 
-# Install python-nautilus
+if [[ "$(whoami)" != root ]]; then
+    exec sudo -- "$0" "$@"
+fi
+
+# python-nautilus indirilecek
 echo "---> python-nautilus dosyası depodan indirilerek kuruluyor..."
-if type "pacman" > /dev/null 2>&1
-then
-    sudo pacman -S --noconfirm python-nautilus
-elif type "apt" > /dev/null 2>&1
-	sleep 3
+if type "apt" > /dev/null 2>&1
 then
     installed=`apt list --installed python-nautilus -qq 2> /dev/null`
     if [ -z "$installed" ]
     then
-        sudo apt install -y python-nautilus
+        apt install -y python-nautilus
     else
         echo "---> python-nautilus zaten kurulu."
-    fi
-elif type "dnf" > /dev/null 2>&1
-	sleep 3
-then
-    installed=`dnf list --installed nautilus-python 2> /dev/null`
-    if [ -z "$installed" ]
-    then
-        sudo dnf install -y nautilus-python > /dev/null 2>&1
-    else
-        echo "---> nautilus-python zaten kurulu."
     fi
 else
     echo "---> python-nautilus dosyası bulunamadı, lütfen manuel olarak kurulum yapın."
 fi
 sleep 2
 
-
-# Remove previous version and setup folder
+# eski kurulum dosyaları silicencek
 echo "---> Önceki sürümler aranıyor ve kaldırılıyor..."
 mkdir -p ~/.local/share/nautilus-python/extensions
 rm -f ~/.local/share/nautilus-python/extensions/backspace-up.py
@@ -39,14 +28,11 @@ rm -f ~/.local/share/nautilus-python/extensions/BackspaceBack.py
 sleep 2
 
 
-# Download and install the extension
+# python eklentisi indirilecek ve ilgili dizine kopyalanacak
 echo "---> Yeni sürüm indiriliyor ve kuruluyor..."
-wget --show-progress -q -nc https://raw.githubusercontent.com/yahyayildirim/backspace/main/backspace-up.py -O ~/.local/share/nautilus-python/extensions/backspace-up.py > /dev/null 2>&1
+wget --show-progress -q -nc https://raw.githubusercontent.com/yahyayildirim/backspace/main/backspace-up.py -O ~/.local/share/nautilus-python/extensions/backspace-up.py
 chmod +xr ~/.local/share/nautilus-python/extensions/backspace-up.py
 sleep 1
-
-
-
 
 # Kurulum tamamlandı
 
@@ -59,7 +45,6 @@ if [ `ps awx | grep nautilus | grep -v grep | wc -l` -eq 0 ]; then
 		exit 0
 	done
 elif [ `ps awx | grep nautilus | grep -v grep | wc -l` == 1 ]; then
-	#statements
 	killall nautilus
 	sleep 2
 	echo '---> Kurulum tamamlandı. Çıkmak için bir tuşa basın.'
